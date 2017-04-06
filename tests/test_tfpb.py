79e94dacc13578f330335ffca6aaa56808bc8622
Joanna <joanna.materzynska@twentybn.com>
2017-04-06 15:13:31 +0200

    abandoning tf.test_session because it only works as context manager

--- tests/test_tfpb.py
+++ tests/test_tfpb.py
@@ -17,6 +17,11 @@ class TestLoading(tf.test.TestCase):
         self.tmp = tf.test.get_temp_dir()
         self.fname = os.path.join(self.tmp, 'test.pb')
         self.graph = tf.Graph()
+        self.session = tf.Session(graph=self.graph)
+
+    def tearDown(self):
+        self.session.close()
+        tf.reset_default_graph()
 
     def get_session(self):
         return self.test_session(graph=self.graph)
@@ -26,22 +31,21 @@ class TestLoading(tf.test.TestCase):
             a = tf.constant([2.])
             x = tf.placeholder(tf.float32, name='input')
             y = tf.multiply(a, x, name='output')
-        with self.get_session() as session:
-            session.run(tfpb.initializer())
+        with self.session.as_default():
             tf.train.write_graph(
-                session.graph_def, self.tmp, 'test.pb', as_text=False)
+                self.session.graph_def, self.tmp, 'test.pb', as_text=False)
         return y
 
     def test_load_protobuf(self):
         y_created = self.create_example()
         y_loaded = list(tfpb.load_protobuf(
             self.fname,
-            session=self.get_session).values())
+            session=self.session).values())
         input_dict = {'input:0': [3.]}
 
-        with self.get_session() as session:
-            direct_result = session.run(y_created, feed_dict=input_dict)
-            loaded_result = session.run(y_loaded, feed_dict=input_dict)
+        with self.session.as_default():
+            direct_result = self.session.run(y_created, feed_dict=input_dict)
+            loaded_result = self.session.run(y_loaded, feed_dict=input_dict)
 
         self.assertAlmostEqual(direct_result, loaded_result)
 
@@ -52,49 +56,65 @@ class TestFreezing(tf.test.TestCase):
         self.tmp = tf.test.get_temp_dir()
         self.fname = os.path.join(self.tmp, 'test.pb')
         self.graph = tf.Graph()
+        self.session = tf.Session(graph=self.graph)
+
+    def tearDown(self):
+        self.session.close()
+        tf.reset_default_graph()
 
     def get_session(self):
         return self.test_session(graph=self.graph)
 
-    def create_graph_def(self):
+    def create_graph_def(self, session, initialized=True):
         with self.graph.as_default():
-            a = tf.Variable(tf.zeros((1,)))
-            x = tf.placeholder(tf.float32, name='input')
-            tf.add(a, x, name='output')
-        with self.get_session() as session:
-            session.run(tfpb.initializer())
-            return session.graph.as_graph_def()
+            with tf.device('/cpu:0'):
+                a = tf.Variable(tf.zeros((1,)))
+                x = tf.placeholder(tf.float32, name='input')
+                tf.add(a, x, name='output')
+                with self.session.as_default():
+                    if initialized:
+                        self.session.run(tfpb.initializer())
+                    return self.session.graph.as_graph_def()
 
     def test_var2const_should_remove_variable_nodes(self):
-        graph_def = self.create_graph_def()
+        with self.session.as_default():
+            graph_def = self.create_graph_def(self.session)
 
-        constant_graph_def = tfpb.var2const(
-            graph_def, ['output'],
-            session=self.get_session)
+            constant_graph_def = tfpb.var2const(
+                graph_def, ['output'],
+                session=self.session)
 
         for node in constant_graph_def.node:
             self.assertNotEqual(node.op, 'Variable')
 
+    def test_var2const_should_raise_if_no_weights(self):
+        with self.session.as_default():
+            graph_def = self.create_graph_def(self.session, initialized=False)
+
+            with self.assertRaises(tfpb.NoWeightsError):
+                tfpb.var2const(graph_def,
+                               ['output'],
+                               session=self.session)          
+
     def test_store_load_gives_identity(self):
         input_dict = {'input:0': [3.]}
-        graph_def = self.create_graph_def()
-        with self.get_session() as session:
+
+        with self.session.as_default():
+            graph_def = self.create_graph_def(self.session)
             tf.import_graph_def(graph_def, name='')
-            session.run(tfpb.initializer())
-            result_before = session.run('output:0', feed_dict=input_dict)
+            result_before = self.session.run('output:0', feed_dict=input_dict)
 
-        # Save freezed graph and load
-        tfpb.save_protobuf(graph_def, self.fname, session=self.get_session)
-        recovered = tfpb.load_protobuf(self.fname, session=self.get_session)
+            # Save freezed graph and load
+            tfpb.save_protobuf(graph_def, self.fname, session=self.session)
+            recovered = tfpb.load_protobuf(self.fname, session=self.session)
 
-        with self.get_session() as session:
-            session.run(tfpb.initializer())
-            result_after = session.run(recovered, feed_dict=input_dict)
+            result_after = self.session.run(recovered, feed_dict=input_dict)
 
         self.assertAlmostEqual(result_before, result_after['output:0'])
 
     def test_clear_devices_removes_all_device_specs(self):
-        graph_def = self.create_graph_def()
+        with self.session.as_default():
+            graph_def = self.create_graph_def(self.session)
 
         # Make sure there is some device information
         self.assertTrue(any([len(node.device) for node in graph_def.node]))
